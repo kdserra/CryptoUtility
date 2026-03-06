a78521c27032e6f000a744b622d95202ae8ac5a0
Kevin Serra <kevindanielserra@gmail.com>
2026-03-06 11:45:14 -0500

    Added backwards compatible implementation

--- CryptoUtility/KeyNormalizer/Pbkdf2KeyNormalizer.cs
+++ CryptoUtility/KeyNormalizer/Pbkdf2KeyNormalizer.cs
@@ -1,5 +1,4 @@
-﻿#if NET8_0_OR_GREATER
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 
 namespace CryptoUtility;
 
@@ -11,27 +10,37 @@ namespace CryptoUtility;
 /// </remarks>
 public sealed class Pbkdf2KeyNormalizer : IKeyNormalizer
 {
+#if NET8_0_OR_GREATER
     private HashAlgorithmName _hashAlgorithm;
+#endif
     private readonly byte[] _salt;
     private readonly int _iterations;
 
     public Pbkdf2KeyNormalizer(
+#if NET8_0_OR_GREATER
         HashAlgorithmName hashAlgorithm,
+#endif
         byte[] salt,
         int iterations = 100_000
     )
     {
+#if NET8_0_OR_GREATER
         _hashAlgorithm = hashAlgorithm;
+#endif
         _salt = salt;
         _iterations = iterations;
     }
 
     public byte[] Normalize(byte[] key, int keySize)
     {
-        var output = new byte[keySize];
+#if NET8_0_OR_GREATER
+        byte[] output = new byte[keySize];
         Rfc2898DeriveBytes.Pbkdf2(key, _salt, output, _iterations, _hashAlgorithm);
+#else
+        using Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(key, _salt, _iterations);
+        byte[] output = pbkdf2.GetBytes(keySize);
+#endif
 
         return output;
     }
 }
-#endif
