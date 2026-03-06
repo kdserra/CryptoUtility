5a7e9f47b4f04e386e69fdcfbdb3da10dc5843be
Kevin Serra <kevindanielserra@gmail.com>
2026-03-05 22:38:43 -0500

    Update ISymmetricCryptor.cs

--- CryptoUtility/SymmetricCryptor/ISymmetricCryptor.cs
+++ CryptoUtility/SymmetricCryptor/ISymmetricCryptor.cs
@@ -14,18 +14,21 @@ public interface ISymmetricCryptor
     public int KeySize { get; }
 
     /// <summary>
-    /// Encrypts the specified value using the provided key and returns the encrypted data as a byte array.
+    /// Encrypts the specified value using the provided key and returns the encrypted data.
     /// </summary>
-    /// <remarks>
-    /// Encryption pads the key if it's too short.
-    /// </remarks>
+    /// <param name="key">The cryptographic key used for encryption.</param>
+    /// <param name="value">The value to encrypt.</param>
+    /// <param name="keyNormalizer">Optional key normalizer. If not provided, a default will be used.</param>
+    /// <returns>The encrypted data.</returns>
     public byte[] Encrypt(byte[] key, byte[] value, IKeyNormalizer? keyNormalizer);
 
     /// <summary>
-    ///
+    /// Decrypts the specified encrypted value using the provided key and returns the decrypted data.
     /// </summary>
-    /// <remarks>
-    /// Decryption pads the key for the key if it's too short.
-    /// </remarks>
+    /// <param name="cryptor">The symmetric cryptor instance.</param>
+    /// <param name="key">The cryptographic key used for decryption.</param>
+    /// <param name="encryptedValue">The encrypted value to decrypt.</param>
+    /// <param name="keyNormalizer">Optional key normalizer. If not provided, a default will be used.</param>
+    /// <returns>The decrypted data.</returns>
     public byte[] Decrypt(byte[] key, byte[] encryptedValue, IKeyNormalizer? keyNormalizer);
 }
