75ce4873c0544ff180c35e38f139b2603689ab5c
Kevin Serra <kevindanielserra@gmail.com>
2026-04-19 12:50:07 -0400

    Update GenerateStaticApiAttribute.cs

--- CryptoUtility/Utility/GenerateStaticApiAttribute.cs
+++ CryptoUtility/Utility/GenerateStaticApiAttribute.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Marks a class to be wrapped with a static class that raises it's public functions and properties as static functions
 /// and properties, by accessing them through a cached reference to the underlying instance. This is used to provide a
-/// more convenient API for users of the library, while still allowing for instance-based usage if desired.
+/// more convenient API for users of the library.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class GenerateStaticApiAttribute : Attribute { }
