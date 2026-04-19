68711c0340ed839385b1d3a7ae715de1002f3710
Kevin Serra <kevindanielserra@gmail.com>
2026-04-19 12:47:46 -0400

    Update GenerateStaticApiAttribute.cs

--- CryptoUtility/Utility/GenerateStaticApiAttribute.cs
+++ CryptoUtility/Utility/GenerateStaticApiAttribute.cs
@@ -1,4 +1,9 @@
 ﻿namespace CryptoUtility;
 
+/// <summary>
+/// Marks a class to be wrapped with a static class that raises it's public functions and properties as static functions
+/// and properties, by accessing them through a cached reference to the underlying instance. This is used to provide a
+/// more convenient API for users of the library, while still allowing for instance-based usage if desired.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class GenerateStaticApiAttribute : Attribute { }
