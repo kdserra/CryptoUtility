f3ccc6d0e22d56f203457d2ab9e52202a2df55d8
Kevin Serra <kevindanielserra@gmail.com>
2026-05-24 00:58:54 -0400

    Add missing tests

---
+++ CryptoUtility.Tests/SymmetricCipher/Aes128GcmTests.cs
@@ -0,0 +1,6 @@
+﻿namespace CryptoUtility.Tests;
+
+public sealed class Aes128GcmTests : SymmetricCipherAEADTests
+{
+    internal override ISymmetricCipher Cipher => new Aes128GcmImpl();
+}
---
+++ CryptoUtility.Tests/SymmetricCipher/Aes192GcmTests.cs
@@ -0,0 +1,6 @@
+﻿namespace CryptoUtility.Tests;
+
+public sealed class Aes192GcmTests : SymmetricCipherAEADTests
+{
+    internal override ISymmetricCipher Cipher => new Aes192GcmImpl();
+}
