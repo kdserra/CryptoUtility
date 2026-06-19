792bec48b88d54ff9b4fd837eef72b6b00936468
Kevin Serra <kevindanielserra@gmail.com>
2026-06-19 11:33:46 -0400

    feat: add Sha256 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/HashProvider/Sha256Impl.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace CryptoUtility.BouncyCastle;
+
+[GenerateStaticApi]
+public sealed class Sha256Impl : IHashProvider
+{
+    public static readonly Sha256Impl Shared = new();
+
+    public byte[] Hash(byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(message);
+
+        Sha256Digest digest = new();
+        digest.BlockUpdate(message, 0, message.Length);
+
+        byte[] result = new byte[digest.GetDigestSize()];
+        digest.DoFinal(result, 0);
+
+        return result;
+    }
+}
---
+++ CryptoUtility.Tests/HashProvider/BouncySha256Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncySha256Impl = CryptoUtility.BouncyCastle.Sha256Impl;
+
+namespace CryptoUtility.Tests;
+
+public sealed class BouncySha256Tests : HashProviderTests
+{
+    internal override IHashProvider HashProvider => new BouncySha256Impl();
+}
