dde2905ac1b93fe49069518971d03c798017726b
Kevin Serra <kevindanielserra@gmail.com>
2026-06-18 20:31:47 -0400

    feat: add Poly1305 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/MacProvider/Poly1305Impl.cs
@@ -0,0 +1,40 @@
+﻿using Org.BouncyCastle.Crypto.Parameters;
+using BouncyPoly1305 = Org.BouncyCastle.Crypto.Macs.Poly1305;
+
+namespace CryptoUtility.BouncyCastle;
+
+/// <summary>
+/// Bouncy Castle Poly1305 MAC Implementation.
+/// </summary>
+[GenerateStaticApi]
+public sealed class Poly1305Impl : IMacProvider
+{
+    public static readonly Poly1305Impl Shared = new();
+
+    public int RequiredKeySizeInBytes => 32;
+
+    public int MacSizeInBytes => 16;
+
+    public byte[] ComputeMac(byte[] key, byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(key, message);
+
+        if (key.Length != RequiredKeySizeInBytes)
+        {
+            throw new ArgumentException(
+                $"Key must be exactly {RequiredKeySizeInBytes} bytes.",
+                nameof(key)
+            );
+        }
+
+        var mac = new BouncyPoly1305();
+        mac.Init(new KeyParameter(key));
+
+        mac.BlockUpdate(message, 0, message.Length);
+
+        byte[] output = new byte[MacSizeInBytes];
+        mac.DoFinal(output, 0);
+
+        return output;
+    }
+}
---
+++ CryptoUtility.Tests/MacProvider/BouncyCastlePoly1305Tests.cs
@@ -0,0 +1,8 @@
+﻿using CryptoUtility.BouncyCastle;
+
+namespace CryptoUtility.Tests;
+
+public class BouncyCastlePoly1305Tests : MacProviderTests
+{
+    internal override IMacProvider Mac { get; } = Poly1305Impl.Shared;
+}
