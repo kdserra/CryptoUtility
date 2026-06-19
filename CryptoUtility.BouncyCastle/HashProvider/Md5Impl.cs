696f23d213cdee63c85dddcb94048cc6c67ceb9c
Kevin Serra <kevindanielserra@gmail.com>
2026-06-18 23:19:12 -0400

    feat: add Md5 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/HashProvider/Md5Impl.cs
@@ -0,0 +1,23 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace CryptoUtility.BouncyCastle;
+
+[GenerateStaticApi]
+public sealed class Md5Impl : IHashProvider
+{
+    public byte[] Hash(byte[] message)
+    {
+        if (message == null)
+        {
+            throw new ArgumentNullException(nameof(message));
+        }
+
+        MD5Digest digest = new();
+        digest.BlockUpdate(message, 0, message.Length);
+
+        byte[] result = new byte[digest.GetDigestSize()];
+        digest.DoFinal(result, 0);
+
+        return result;
+    }
+}
---
+++ CryptoUtility.Tests/HashProvider/BouncyMd5Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncyMd5Impl = CryptoUtility.BouncyCastle.Md5Impl;
+
+namespace CryptoUtility.Tests;
+
+public sealed class BouncyMd5Tests : HashProviderTests
+{
+    internal override IHashProvider HashProvider => new BouncyMd5Impl();
+}
