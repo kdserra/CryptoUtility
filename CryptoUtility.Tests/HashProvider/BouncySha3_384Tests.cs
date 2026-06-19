1a1288403e971bf7299722a14e280c3b6cbb9e38
Kevin Serra <kevindanielserra@gmail.com>
2026-06-19 11:42:40 -0400

    feat: add Sha3_384 to BouncyCastle wrapper

---
+++ CryptoUtility.Tests/HashProvider/BouncySha3_384Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncySha3_384Impl = CryptoUtility.BouncyCastle.Sha3_384Impl;
+
+namespace CryptoUtility.Tests;
+
+public sealed class BouncySha3_384Tests : HashProviderTests
+{
+    internal override IHashProvider HashProvider => new BouncySha3_384Impl();
+}
