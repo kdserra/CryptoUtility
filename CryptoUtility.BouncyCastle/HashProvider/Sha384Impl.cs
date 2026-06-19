5d7fec155b1898e571269cb7866a94b0060dfde1
Kevin Serra <kevindanielserra@gmail.com>
2026-06-19 11:33:57 -0400

    feat: add Sha384 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/HashProvider/Sha384Impl.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace CryptoUtility.BouncyCastle;
+
+[GenerateStaticApi]
+public sealed class Sha384Impl : IHashProvider
+{
+    public static readonly Sha384Impl Shared = new();
+
+    public byte[] Hash(byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(message);
+
+        Sha384Digest digest = new();
+        digest.BlockUpdate(message, 0, message.Length);
+
+        byte[] result = new byte[digest.GetDigestSize()];
+        digest.DoFinal(result, 0);
+
+        return result;
+    }
+}
---
+++ CryptoUtility.Tests/HashProvider/BouncySha384Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncySha384Impl = CryptoUtility.BouncyCastle.Sha384Impl;
+
+namespace CryptoUtility.Tests;
+
+public sealed class BouncySha384Tests : HashProviderTests
+{
+    internal override IHashProvider HashProvider => new BouncySha384Impl();
+}
