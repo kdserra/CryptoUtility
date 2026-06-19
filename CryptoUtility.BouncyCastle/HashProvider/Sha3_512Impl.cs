1065c984e3b9900754a86a2a73da690459a51417
Kevin Serra <kevindanielserra@gmail.com>
2026-06-19 11:42:45 -0400

    feat: add Sha3_512 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/HashProvider/Sha3_512Impl.cs
@@ -0,0 +1,25 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace CryptoUtility.BouncyCastle;
+
+/// <summary>
+/// Bouncy Castle SHA3_512 Implementation.
+/// </summary>
+[GenerateStaticApi]
+public sealed class Sha3_512Impl : IHashProvider
+{
+    public static readonly Sha3_512Impl Shared = new();
+
+    public byte[] Hash(byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(message);
+
+        Sha3Digest digest = new(512);
+        digest.BlockUpdate(message, 0, message.Length);
+
+        byte[] result = new byte[digest.GetDigestSize()];
+        digest.DoFinal(result, 0);
+
+        return result;
+    }
+}
