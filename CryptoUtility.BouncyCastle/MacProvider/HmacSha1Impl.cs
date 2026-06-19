f7123c0b8a012be637483eea37d3ff7cbddd2905
Kevin Serra <kevindanielserra@gmail.com>
2026-06-18 23:22:12 -0400

    feat: add HmacSha1 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/MacProvider/HmacSha1Impl.cs
@@ -0,0 +1,33 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Parameters;
+using BouncyHmac = Org.BouncyCastle.Crypto.Macs.HMac;
+
+namespace CryptoUtility.BouncyCastle;
+
+/// <summary>
+/// Bouncy Castle HMAC-SHA1 Implementation.
+/// </summary>
+[GenerateStaticApi]
+public sealed class HmacSha1Impl : IMacProvider
+{
+    public static readonly HmacSha1Impl Shared = new();
+
+    public int RequiredKeySizeInBytes => 0;
+
+    public int MacSizeInBytes => 20;
+
+    public byte[] ComputeMac(byte[] key, byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(key, message);
+
+        var hmac = new BouncyHmac(new Sha1Digest());
+        hmac.Init(new KeyParameter(key));
+
+        hmac.BlockUpdate(message, 0, message.Length);
+
+        byte[] output = new byte[MacSizeInBytes];
+        hmac.DoFinal(output, 0);
+
+        return output;
+    }
+}
---
+++ CryptoUtility.Tests/MacProvider/BouncyHmacSha1Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncyHmacSha1Impl = CryptoUtility.BouncyCastle.HmacSha1Impl;
+
+namespace CryptoUtility.Tests;
+
+public class BouncyHmacSha1Tests : MacProviderTests
+{
+    internal override IMacProvider Mac { get; } = BouncyHmacSha1Impl.Shared;
+}
