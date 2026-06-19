ba07bad7538ffbf9e1567a3b3635da1dd2847f99
Kevin Serra <kevindanielserra@gmail.com>
2026-06-19 11:34:06 -0400

    feat: add Sha512 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/HashProvider/Sha512Impl.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+
+namespace CryptoUtility.BouncyCastle;
+
+[GenerateStaticApi]
+public sealed class Sha512Impl : IHashProvider
+{
+    public static readonly Sha512Impl Shared = new();
+
+    public byte[] Hash(byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(message);
+
+        Sha512Digest digest = new();
+        digest.BlockUpdate(message, 0, message.Length);
+
+        byte[] result = new byte[digest.GetDigestSize()];
+        digest.DoFinal(result, 0);
+
+        return result;
+    }
+}
---
+++ CryptoUtility.Tests/HashProvider/BouncySha512Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncySha512Impl = CryptoUtility.BouncyCastle.Sha512Impl;
+
+namespace CryptoUtility.Tests;
+
+public sealed class BouncySha512Tests : HashProviderTests
+{
+    internal override IHashProvider HashProvider => new BouncySha512Impl();
+}
