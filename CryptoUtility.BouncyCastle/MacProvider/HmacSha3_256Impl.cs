c13586c0e604ef45d2fd9770071b6992476319a6
Kevin Serra <kevindanielserra@gmail.com>
2026-06-18 23:41:15 -0400

    feat: add HmacSha3_256 to BouncyCastle wrapper

---
+++ CryptoUtility.BouncyCastle/MacProvider/HmacSha3_256Impl.cs
@@ -0,0 +1,33 @@
+﻿using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Parameters;
+using BouncyHmac = Org.BouncyCastle.Crypto.Macs.HMac;
+
+namespace CryptoUtility.BouncyCastle;
+
+/// <summary>
+/// Bouncy Castle HMAC-SHA3_256 Implementation.
+/// </summary>
+[GenerateStaticApi]
+public sealed class HmacSha3_256Impl : IMacProvider
+{
+    public static readonly HmacSha3_256Impl Shared = new();
+
+    public int RequiredKeySizeInBytes => 0;
+
+    public int MacSizeInBytes => 32;
+
+    public byte[] ComputeMac(byte[] key, byte[] message)
+    {
+        LibraryHelper.ThrowIfAnyNull(key, message);
+
+        var hmac = new BouncyHmac(new Sha3Digest(256));
+        hmac.Init(new KeyParameter(key));
+
+        hmac.BlockUpdate(message, 0, message.Length);
+
+        byte[] output = new byte[MacSizeInBytes];
+        hmac.DoFinal(output, 0);
+
+        return output;
+    }
+}
---
+++ CryptoUtility.Tests/MacProvider/BouncyHmacSha3_256Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncyHmacSha3_256Impl = CryptoUtility.BouncyCastle.HmacSha3_256Impl;
+
+namespace CryptoUtility.Tests;
+
+public class BouncyHmacSha3_256Tests : MacProviderTests
+{
+    internal override IMacProvider Mac { get; } = BouncyHmacSha3_256Impl.Shared;
+}
