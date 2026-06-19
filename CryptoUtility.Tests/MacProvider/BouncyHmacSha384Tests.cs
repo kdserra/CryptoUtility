a9af08fae33f74123ac898ccdc29a648e6e5dea1
Kevin Serra <kevindanielserra@gmail.com>
2026-06-18 23:30:50 -0400

    feat: add HmacSha384 to BouncyCastle wrapper

---
+++ CryptoUtility.Tests/MacProvider/BouncyHmacSha384Tests.cs
@@ -0,0 +1,8 @@
+﻿using BouncyHmacSha384Impl = CryptoUtility.BouncyCastle.HmacSha384Impl;
+
+namespace CryptoUtility.Tests;
+
+public class BouncyHmacSha384Tests : MacProviderTests
+{
+    internal override IMacProvider Mac { get; } = BouncyHmacSha384Impl.Shared;
+}
