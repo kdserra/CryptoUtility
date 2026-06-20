d5ed0354fdf601844a57d012251365943a31ef2e
Kevin Serra <kevindanielserra@gmail.com>
2026-06-20 12:30:06 -0400

    feat: add Try extensions to digital sig

--- CryptoUtility/DigitalSignature/DigitalSignatureExtensions.cs
+++ CryptoUtility/DigitalSignature/DigitalSignatureExtensions.cs
@@ -25,12 +25,19 @@ public static class DigitalSignatureExtensions
         string publicKey
     )
     {
-        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-        byte[] signatureBytes = Convert.FromBase64String(signature);
-        byte[] publicKeyBytes = Convert.FromBase64String(publicKey);
-        bool isValid = digitalSignature.Verify(messageBytes, signatureBytes, publicKeyBytes);
+        try
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] signatureBytes = Convert.FromBase64String(signature);
+            byte[] publicKeyBytes = Convert.FromBase64String(publicKey);
+            bool isValid = digitalSignature.Verify(messageBytes, signatureBytes, publicKeyBytes);
 
-        return isValid;
+            return isValid;
+        }
+        catch
+        {
+            return false;
+        }
     }
 
     public static (string publicKey, string secretKey) GenerateKeyPairBase64(
@@ -40,6 +47,95 @@ public static class DigitalSignatureExtensions
         (byte[] publicKeyBytes, byte[] secretKeyBytes) = digitalSignature.GenerateKeyPair();
         string publicKeyBase64 = Convert.ToBase64String(publicKeyBytes);
         string secretKeyBase64 = Convert.ToBase64String(secretKeyBytes);
+
         return (publicKeyBase64, secretKeyBase64);
+    }
+
+    public static bool TrySign(
+        this IDigitalSignature digitalSignature,
+        byte[] message,
+        byte[] secretKey,
+        out byte[] signature
+    )
+    {
+        try
+        {
+            signature = digitalSignature.Sign(message, secretKey);
+
+            return true;
+        }
+        catch
+        {
+            signature = Array.Empty<byte>();
+
+            return false;
+        }
+    }
+
+    public static bool TrySignBase64(
+        this IDigitalSignature digitalSignature,
+        string message,
+        string secretKey,
+        out string signature
+    )
+    {
+        try
+        {
+            signature = digitalSignature.SignBase64(message, secretKey);
+
+            return true;
+        }
+        catch
+        {
+            signature = string.Empty;
+
+            return false;
+        }
+    }
+
+    public static bool TryGenerateKeyPair(
+        this IDigitalSignature digitalSignature,
+        out byte[] publicKey,
+        out byte[] secretKey
+    )
+    {
+        try
+        {
+            (byte[] publicKey, byte[] secretKey) keyPair = digitalSignature.GenerateKeyPair();
+            publicKey = keyPair.publicKey;
+            secretKey = keyPair.secretKey;
+
+            return true;
+        }
+        catch
+        {
+            publicKey = Array.Empty<byte>();
+            secretKey = Array.Empty<byte>();
+
+            return false;
+        }
+    }
+
+    public static bool TryGenerateKeyPairBase64(
+        this IDigitalSignature digitalSignature,
+        out string publicKey,
+        out string secretKey
+    )
+    {
+        try
+        {
+            (string publicKey, string secretKey) result = digitalSignature.GenerateKeyPairBase64();
+            publicKey = result.publicKey;
+            secretKey = result.secretKey;
+
+            return true;
+        }
+        catch
+        {
+            publicKey = string.Empty;
+            secretKey = string.Empty;
+
+            return false;
+        }
     }
 }
