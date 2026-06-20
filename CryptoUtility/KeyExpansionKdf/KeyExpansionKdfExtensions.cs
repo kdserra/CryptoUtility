5b63da0cfe0e47a2be92822110e98f234b9b6c10
Kevin Serra <kevindanielserra@gmail.com>
2026-06-20 13:17:41 -0400

    feat: add Base64 util and Try to KeyExpansionKdf

--- CryptoUtility/KeyExpansionKdf/KeyExpansionKdfExtensions.cs
+++ CryptoUtility/KeyExpansionKdf/KeyExpansionKdfExtensions.cs
@@ -6,6 +6,37 @@ public static class KeyExpansionKdfExtensions
 {
     public static string DeriveKeyBase64(
         this IKeyExpansionKdf kdf,
+        string inputKeyMaterial,
+        int iterations,
+        int outputLength,
+        string salt,
+        string info
+    )
+    {
+        byte[] inputKeyMaterialBytes = Convert.FromBase64String(inputKeyMaterial);
+        byte[] saltBytes = Convert.FromBase64String(salt);
+        byte[] infoBytes = Convert.FromBase64String(info);
+
+        byte[] keyBytes = kdf.DeriveKey(
+            inputKeyMaterialBytes,
+            iterations,
+            outputLength,
+            saltBytes,
+            infoBytes
+        );
+
+        string keyBase64 = Convert.ToBase64String(keyBytes);
+
+        CryptographicOperations.ZeroMemory(inputKeyMaterialBytes);
+        CryptographicOperations.ZeroMemory(saltBytes);
+        CryptographicOperations.ZeroMemory(infoBytes);
+        CryptographicOperations.ZeroMemory(keyBytes);
+
+        return keyBase64;
+    }
+
+    public static string DeriveKeyBase64(
+        this IKeyExpansionKdf kdf,
         byte[] inputKeyMaterial,
         int iterations,
         int outputLength,
@@ -19,5 +50,89 @@ public static class KeyExpansionKdfExtensions
         CryptographicOperations.ZeroMemory(keyBytes);
 
         return keyBase64;
+    }
+
+    public static bool TryDeriveKey(
+        this IKeyExpansionKdf kdf,
+        byte[] inputKeyMaterial,
+        int iterations,
+        int outputLength,
+        byte[] salt,
+        byte[] info,
+        out byte[] derivedKey
+    )
+    {
+        try
+        {
+            derivedKey = kdf.DeriveKey(inputKeyMaterial, iterations, outputLength, salt, info);
+
+            return true;
+        }
+        catch
+        {
+            derivedKey = Array.Empty<byte>();
+
+            return false;
+        }
+    }
+
+    public static bool TryDeriveKeyBase64(
+        this IKeyExpansionKdf kdf,
+        string inputKeyMaterial,
+        int iterations,
+        int outputLength,
+        string salt,
+        string info,
+        out string derivedKey
+    )
+    {
+        try
+        {
+            derivedKey = kdf.DeriveKeyBase64(
+                inputKeyMaterial,
+                iterations,
+                outputLength,
+                salt,
+                info
+            );
+
+            return true;
+        }
+        catch
+        {
+            derivedKey = string.Empty;
+
+            return false;
+        }
+    }
+
+    public static bool TryDeriveKeyBase64(
+        this IKeyExpansionKdf kdf,
+        byte[] inputKeyMaterial,
+        int iterations,
+        int outputLength,
+        byte[] salt,
+        byte[] info,
+        out string derivedKey
+    )
+    {
+        try
+        {
+            derivedKey = kdf.DeriveKeyBase64(
+                inputKeyMaterial,
+                iterations,
+                outputLength,
+                salt,
+                info
+            );
+
+            return true;
+        }
+        catch
+        {
+            derivedKey = string.Empty;
+
+            return false;
+        }
     }
 }
