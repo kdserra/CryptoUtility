d9d0d7c6546ccc4a3675305330756d420e38c87e
Kevin Serra <kevindanielserra@gmail.com>
2026-06-20 12:14:02 -0400

    feat: add Try extensions to asymmetric cipher

--- CryptoUtility/AsymmetricCipher/AsymmetricCipherExtensions.cs
+++ CryptoUtility/AsymmetricCipher/AsymmetricCipherExtensions.cs
@@ -139,4 +139,183 @@ public static class AsymmetricCipherExtensions
         string plaintext = Encoding.UTF8.GetString(plaintextBytes);
         return plaintext;
     }
+
+    public static bool TryEncrypt(
+        this IAsymmetricCipher cipher,
+        byte[] publicKey,
+        byte[] plaintext,
+        out byte[] encrypted
+    )
+    {
+        try
+        {
+            encrypted = cipher.Encrypt(publicKey, plaintext);
+            return true;
+        }
+        catch
+        {
+            encrypted = Array.Empty<byte>();
+            return false;
+        }
+    }
+
+    public static bool TryDecrypt(
+        this IAsymmetricCipher cipher,
+        byte[] secretKey,
+        byte[] encrypted,
+        out byte[] plaintext
+    )
+    {
+        try
+        {
+            plaintext = cipher.Decrypt(secretKey, encrypted);
+            return true;
+        }
+        catch
+        {
+            plaintext = Array.Empty<byte>();
+            return false;
+        }
+    }
+
+    public static bool TryEncryptBase64(
+        this IAsymmetricCipher cipher,
+        string publicKey,
+        string plaintext,
+        out string encrypted
+    )
+    {
+        try
+        {
+            encrypted = cipher.EncryptBase64(publicKey, plaintext);
+            return true;
+        }
+        catch
+        {
+            encrypted = string.Empty;
+            return false;
+        }
+    }
+
+    public static bool TryGenerateKeyPair(
+        this IAsymmetricCipher cipher,
+        out byte[] publicKey,
+        out byte[] secretKey
+    )
+    {
+        try
+        {
+            (byte[] publicKey, byte[] secretKey) keyPair = cipher.GenerateKeyPair();
+            publicKey = keyPair.publicKey;
+            secretKey = keyPair.secretKey;
+            return true;
+        }
+        catch
+        {
+            publicKey = Array.Empty<byte>();
+            secretKey = Array.Empty<byte>();
+            return false;
+        }
+    }
+
+    public static bool TryGenerateKeyPairBase64(
+        this IAsymmetricCipher cipher,
+        out string publicKey,
+        out string secretKey
+    )
+    {
+        try
+        {
+            (string publicKey, string secretKey) keyPair = cipher.GenerateKeyPairBase64();
+            publicKey = keyPair.publicKey;
+            secretKey = keyPair.secretKey;
+            return true;
+        }
+        catch
+        {
+            publicKey = string.Empty;
+            secretKey = string.Empty;
+            return false;
+        }
+    }
+
+    public static bool TryHybridEncrypt(
+        this IAsymmetricCipher asymmetricCipher,
+        ISymmetricCipher symmetricCipher,
+        byte[] publicKey,
+        byte[] plaintext,
+        out byte[] encrypted
+    )
+    {
+        try
+        {
+            encrypted = asymmetricCipher.HybridEncrypt(symmetricCipher, publicKey, plaintext);
+            return true;
+        }
+        catch
+        {
+            encrypted = Array.Empty<byte>();
+            return false;
+        }
+    }
+
+    public static bool TryHybridDecrypt(
+        this IAsymmetricCipher asymmetricCipher,
+        ISymmetricCipher symmetricCipher,
+        byte[] secretKey,
+        byte[] encrypted,
+        out byte[] plaintext
+    )
+    {
+        try
+        {
+            plaintext = asymmetricCipher.HybridDecrypt(symmetricCipher, secretKey, encrypted);
+            return true;
+        }
+        catch
+        {
+            plaintext = Array.Empty<byte>();
+            return false;
+        }
+    }
+
+    public static bool TryHybridEncryptBase64(
+        this IAsymmetricCipher asymmetricCipher,
+        ISymmetricCipher symmetricCipher,
+        string publicKey,
+        string plaintext,
+        out string encrypted
+    )
+    {
+        try
+        {
+            encrypted = asymmetricCipher.HybridEncryptBase64(symmetricCipher, publicKey, plaintext);
+            return true;
+        }
+        catch
+        {
+            encrypted = string.Empty;
+            return false;
+        }
+    }
+
+    public static bool TryHybridDecryptBase64(
+        this IAsymmetricCipher asymmetricCipher,
+        ISymmetricCipher symmetricCipher,
+        string secretKey,
+        string encrypted,
+        out string plaintext
+    )
+    {
+        try
+        {
+            plaintext = asymmetricCipher.HybridDecryptBase64(symmetricCipher, secretKey, encrypted);
+            return true;
+        }
+        catch
+        {
+            plaintext = string.Empty;
+            return false;
+        }
+    }
 }
