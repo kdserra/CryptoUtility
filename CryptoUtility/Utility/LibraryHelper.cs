db7bac3b105a2f5737c564db037c2dbb0018b01d
Kevin Serra <kevindanielserra@gmail.com>
2026-06-22 16:31:50 -0400

    chore: cleanup, remove unused functions

--- CryptoUtility/Utility/LibraryHelper.cs
+++ CryptoUtility/Utility/LibraryHelper.cs
@@ -1,42 +1,14 @@
-﻿namespace CryptoUtility;
+namespace CryptoUtility;
 
 internal static class LibraryHelper
 {
-    internal static bool NotNull(params object?[] objects)
-    {
-        foreach (object? obj in objects)
-        {
-            if (obj == null)
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    internal static bool NotNullOrEmpty(params object?[] objects)
-    {
-        foreach (var obj in objects)
-        {
-            if (obj == null)
-                return false;
-
-            if (obj is string str && string.IsNullOrEmpty(str))
-                return false;
-
-            if (
-                obj is System.Collections.IEnumerable enumerable
-                && !enumerable.Cast<object>().Any()
-            )
-                return false;
-        }
-
-        return true;
-    }
-
     internal static void ThrowIfAnyNull(params object?[] objects)
     {
+        if (objects == null)
+        {
+            throw new ArgumentNullException();
+        }
+
         foreach (object? obj in objects)
         {
             if (obj == null)
